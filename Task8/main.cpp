b6b7592e8e5e2d5598f77b6361d43f796cfd349c
George <george.hawtin@students.plymouth.ac.uk>
2021-12-07 15:47:35 +0000

    07/12/2021 (2)

--- Task8/main.cpp
+++ Task8/main.cpp
@@ -26,7 +26,7 @@ void displayArray(int rows, int cols, double *Array)
 }
 
 // ***** For (1) WRITE YOUR ARRAY Y HERE *****
-double y[4];
+double y[3];
 // ***** For (3) WRITE YOUR FUNCTION HERE *****
 
 // *****  END YOUR FUNCTION HERE  *****
@@ -42,8 +42,32 @@ int main()
     double y1 = ((M[0][0]*N[0][0]) + (M[0][1]*N[1][0]) + (M[0][2]*N[2][0]))   *   ((M[0][0]*N[0][1]) + (M[0][1]*N[1][1]) + (M[0][2]*N[2][1]))   *   ((M[0][0]*N[0][2]) + (M[0][1]*N[1][2]) + (M[0][2]*N[2][2]));
     double y2 = ((M[1][0]*N[0][0]) + (M[1][1]*N[1][0]) + (M[1][2]*N[2][0]))   *   ((M[1][0]*N[0][1]) + (M[1][1]*N[1][1]) + (M[1][2]*N[2][1]))   *   ((M[1][0]*N[0][2]) + (M[1][1]*N[1][2]) + (M[1][2]*N[2][2]));
     double y3 = ((M[2][0]*N[0][0]) + (M[2][1]*N[1][0]) + (M[2][2]*N[2][0]))   *   ((M[2][0]*N[0][1]) + (M[2][1]*N[1][1]) + (M[2][2]*N[2][1]))   *   ((M[2][0]*N[0][2]) + (M[2][1]*N[1][2]) + (M[2][2]*N[2][2]));
-    // 2. Write a nested loop to perform a matrix multiplication M*N and store the result in Y
+    y[0] = y1;
+    y[1] = y2;
+    y[2] = y3;
+    for(unsigned short i = 0; i!=3; i++)
+    {
+        printf("without out the nested loop it is = %f\n", y[i]);
+    }
 
+    // 2. Write a nested loop to perform a matrix multiplication M*N and store the result in Y
+    double x;
+    double X = 1;
+    for(unsigned short z = 0; z!=3; z++)
+    {
+        
+        for(unsigned short n = 0; n!=3; n++)
+        {
+            for(unsigned short m = 0; m!=3; m++)
+            {
+                x = ((M[n][0]*N[0][0]) + (M[n][1]*N[1][0]) + (M[n][2]*N[2][0]));
+                printf("x = %f\n", x);
+            }
+            X = X*x;
+        }
+        y[z] = X;
+        printf("array y = %f\n", y[z]);
+    }
     // 3. Write a function to multiply two matrices together (see comments above). Include some text code to demonstrate it working
 
     // ***** MODIFY THE CODE ABOVE HERE *****
