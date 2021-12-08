a55ac7010e169b60ae2d53b956bdc5734ef53079
George <george.hawtin@students.plymouth.ac.uk>
2021-12-08 14:52:33 +0000

    08/12/2021

--- Task8/main.cpp
+++ Task8/main.cpp
@@ -2,17 +2,14 @@
 using namespace uop_msb;
 
 // 4x4 Array
-double M[4][3] = {
-    {1.0, 2.0, 3.0},
-    {-3.0, -4.0, -5.0},
-    {10.0, 11.0, 12.0},
-    {-1.0, -2.0, -3.0}
+double M[4][3] = 
+{
+    {1.0, 2.0, 3.0},  {-3.0, -4.0, -5.0},  {10.0, 11.0, 12.0},  {-1.0, -2.0, -3.0}
 };
 
-double N[3][4] = {
-    {1.0, 2.0, 3.0, 4.0},
-    {-3.0, -4.0, -5.0, -6.0},
-    {10.0, 11.0, 12.0, 13.0}
+double N[3][4] = 
+{
+    {1.0, 2.0, 3.0, 4.0},  {-3.0, -4.0, -5.0, -6.0},  {10.0, 11.0, 12.0, 13.0}
 };
 
 void displayArray(int rows, int cols, double *Array)
@@ -26,7 +23,7 @@ void displayArray(int rows, int cols, double *Array)
 }
 
 // ***** For (1) WRITE YOUR ARRAY Y HERE *****
-double y[3];
+double y[4][4];
 // ***** For (3) WRITE YOUR FUNCTION HERE *****
 
 // *****  END YOUR FUNCTION HERE  *****
@@ -39,32 +36,38 @@ int main()
     // ***** MODIFY THE CODE BELOW HERE *****
 
     // 1. Create another Array Y with the correct dimensions to hold the result of M*N (see  comments above)
-    double y1 = ((M[0][0]*N[0][0]) + (M[0][1]*N[1][0]) + (M[0][2]*N[2][0]))   *   ((M[0][0]*N[0][1]) + (M[0][1]*N[1][1]) + (M[0][2]*N[2][1]))   *   ((M[0][0]*N[0][2]) + (M[0][1]*N[1][2]) + (M[0][2]*N[2][2]));
-    double y2 = ((M[1][0]*N[0][0]) + (M[1][1]*N[1][0]) + (M[1][2]*N[2][0]))   *   ((M[1][0]*N[0][1]) + (M[1][1]*N[1][1]) + (M[1][2]*N[2][1]))   *   ((M[1][0]*N[0][2]) + (M[1][1]*N[1][2]) + (M[1][2]*N[2][2]));
-    double y3 = ((M[2][0]*N[0][0]) + (M[2][1]*N[1][0]) + (M[2][2]*N[2][0]))   *   ((M[2][0]*N[0][1]) + (M[2][1]*N[1][1]) + (M[2][2]*N[2][1]))   *   ((M[2][0]*N[0][2]) + (M[2][1]*N[1][2]) + (M[2][2]*N[2][2]));
-    y[0] = y1;
-    y[1] = y2;
-    y[2] = y3;
-    for(unsigned short i = 0; i!=3; i++)
-    {
-        printf("without out the nested loop it is = %f\n", y[i]);
-    }
 
     // 2. Write a nested loop to perform a matrix multiplication M*N and store the result in Y
-    double x;
-    double X;
-    for(unsigned short z = 0; z!=3; z++)
+    
+    
+    //rows in M
+    for(int g = 0; g < 4; g++)
     {
-        X=1;
-        for(unsigned short n = 0; n!=3; n++)
+        //colums in M
+        for(int h = 0; h < 3 ; h++)
         {
-            x = ((M[n][0]*N[0][0]) + (M[n][1]*N[1][0]) + (M[n][2]*N[2][0]));
-            X = x;
-            printf("X = %f\n", X);
+            //colums in N
+            for(int j = 0; j<4 ; j++)
+            {
+                //god i love maths *crying sounds*
+                y[g][h] += M[g][j]*N[j][h];
+                printf("y[%i][%i] =  %f\n", g, h, y[g][h]);
+            }
         }
-        y[z] = X;
-        printf("array y = %f\n", y[z]);
     }
+
+    printf("\n");
+    printf("\n");
+    printf("\n");
+
+    for(int k; k < 4; k++)
+    {
+        for(int l; l < 4; l++)
+        {
+            printf("y[%i][%i] =  %f\n", k, l, y[k][l]);
+        }
+    }
+    
     // 3. Write a function to multiply two matrices together (see comments above). Include some text code to demonstrate it working
 
     // ***** MODIFY THE CODE ABOVE HERE *****
